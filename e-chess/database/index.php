ed961f4a628e0b00d07a877412af207531fb04d8
jan-patrick <jan.schneider@hfg-gmuend.de>
2018-04-26 14:40:20 +0200

    e-chess in title on website

--- e-chess/database/index.php
+++ e-chess/database/index.php
@@ -19,7 +19,7 @@ if (!$query) {
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<title>DATABASE - Jan Schneider</title>
+<title>database - E-CHESS</title>
 <link rel="shortcut icon" href="../echess.png">
 <link rel="icon" type="image/png" href="../echess.svg">
 <link rel="image_src" href="../echess.svg" />
