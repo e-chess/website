2be4aeb4beda45e9f3a14f659ae4fb3797b21d5e
jan-patrick <jan.schneider@hfg-gmuend.de>
2018-08-09 17:31:21 +0200

    working on buy button

--- buy.php
+++ buy.php
@@ -1,10 +1,12 @@
 <?php
 
+    $data = json_decode(file_get_contents('php://input'), true);
+    
     $to = "mail@jan-patrick.de";
-    $from = $_POST['email'];
-    $name = $_POST['name'];
+    $from = $data['from'];
+    $name = $data['name'];
     $header = "Neue Nachricht von $from";
-    $subject = $_POST['subject'];
+    $subject = $data['subject'];
 
     $send = mail($to, $subject, $body, $header);
 
