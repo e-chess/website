30678f958509d7c76800a35e80a3bb693bb37601
Jan Schneider <jan.schneider@hfg-gmuend.de>
2018-09-14 17:45:41 +0200

    fully working online game

--- play/index.php
+++ play/index.php
@@ -1,12 +1,4 @@
 <?php
-require_once "../data.php";
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-};
-
 
 function updateNewest($conn, $state){
     
